3298a007c4484f9422bcb1cabe54b3676610c74b
eigen-cascadeo <36188137+eigen-cascadeo@users.noreply.github.com>
2018-02-23 23:51:53 +0800

    Specify hono build target directory.

--- hono-ci.sh
+++ hono-ci.sh
@@ -2,4 +2,4 @@
 
 mvn clean install -Ddocker.host=tcp://172.18.0.1:2375 -Pbuild-docker-image -f hono/pom.xml
 mkdir -p /opt/hono
-cp -pr example/target /opt/hono
+cp -pr /hono/example/target /opt/hono
