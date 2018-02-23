4a418b935a8c36fbda8167c233d89dca38035eac
eigen-cascadeo <36188137+eigen-cascadeo@users.noreply.github.com>
2018-02-24 00:27:52 +0800

    Update hono-ci.sh

--- hono-ci.sh
+++ hono-ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mvn clean install -Ddocker.host=tcp://172.18.0.1:2375 -Pbuild-docker-image -f hono/pom.xml
-mkdir -p /opt/hono
-cp -pr /hono/example/target /opt/hono
+ls /usr/local/src
+mkdir -p /usr/local/src/hono
+cp -pr /hono/example/target /usr/local/src/hono/
+ls /usr/local/src
