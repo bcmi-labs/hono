1212c20b8457c8a53f7739c7e9b314d0924f39ea
eigen-cascadeo <36188137+eigen-cascadeo@users.noreply.github.com>
2018-02-23 23:30:34 +0800

    hono-ci: specify location of pom.xml

--- hono-ci.sh
+++ hono-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn clean install -Ddocker.host=tcp://172.18.0.1:2375 -Pbuild-docker-image
+mvn clean install -Ddocker.host=tcp://172.18.0.1:2375 -Pbuild-docker-image -f hono/pom.xml
 mkdir -p /opt/hono
 cp -pr example/target /opt/hono
